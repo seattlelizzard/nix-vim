7ab69efeec996b302ffef89c3d92176396232b20
= <=>
2024-09-16 21:00:15 -0700

    weoeowowowowo

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,6 +44,11 @@ opt.cmdheight = 0
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
+vim.cmd("colorscheme catppuccin-macchiato") -- set color theme
+require("bufferline").setup()
+
+
+
 -- Configure Neovim diagnostic messages
 
 local function prefix_diagnostic(prefix, diagnostic)
